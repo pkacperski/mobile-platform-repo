83f5d26388f17183dab8a404a4ab2faac6ee0ac7
pkacperski <przemyslaw.kacperski@interia.pl>
2021-12-03 12:00:19 +0100

    Added @NotNull exception handling when performing a POST on data objects without vehicleId - changes to pom.xml and schema.sql

--- mobileplatform-backend/src/main/resources/schema.sql
+++ mobileplatform-backend/src/main/resources/schema.sql
@@ -6,13 +6,20 @@ DROP TABLE IF EXISTS public.location;
 DROP TABLE IF EXISTS public.point_cloud;
 DROP TABLE IF EXISTS public.vehicle;
 
+CREATE TABLE public.vehicle (
+    id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    vehicle_name varchar(255),
+    connection_date timestamp without time zone
+);
+
 CREATE TABLE public.diagnostic_data (
     id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
     vehicle_id bigint,
     reading_date timestamp without time zone,
     battery_charge_status real,
     camera_turn_angle real,
-    wheels_turn_measure real
+    wheels_turn_measure real,
+    foreign key (vehicle_id) references public.vehicle(id)
 );
 
 CREATE TABLE public.encoder_reading (
@@ -22,7 +29,8 @@ CREATE TABLE public.encoder_reading (
     left_front_wheel_speed double precision,
     left_rear_wheel_speed double precision,
     right_front_wheel_speed double precision,
-    right_rear_wheel_speed double precision
+    right_rear_wheel_speed double precision,
+    foreign key (vehicle_id) references public.vehicle(id)
 );
 
 CREATE TABLE public.imu_reading (
@@ -37,14 +45,16 @@ CREATE TABLE public.imu_reading (
     angular_velocity_z double precision,
     magnetic_field_x double precision,
     magnetic_field_y double precision,
-    magnetic_field_z double precision
+    magnetic_field_z double precision,
+    foreign key (vehicle_id) references public.vehicle(id)
 );
 
 CREATE TABLE public.lidar_reading (
     id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
     vehicle_id bigint,
     reading_date timestamp without time zone,
-    lidar_distances_reading varchar(16384)
+    lidar_distances_reading varchar(16384),
+    foreign key (vehicle_id) references public.vehicle(id)
  );
 
 CREATE TABLE public.location (
@@ -55,18 +65,14 @@ CREATE TABLE public.location (
     real_y_coordinate double precision,
     slam_rotation double precision,
     slam_x_coordinate double precision,
-    slam_y_coordinate double precision
+    slam_y_coordinate double precision,
+    foreign key (vehicle_id) references public.vehicle(id)
 );
 
 CREATE TABLE public.point_cloud (
     id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
     vehicle_id bigint,
     reading_date timestamp without time zone,
-    point_cloud_reading varchar(1048576) --PostgreSQL: text
-);
-
-CREATE TABLE public.vehicle (
-    id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    vehicle_name varchar(255),
-    connection_date timestamp without time zone
+    point_cloud_reading varchar(1048576), --PostgreSQL: text
+    foreign key (vehicle_id) references public.vehicle(id)
 );
