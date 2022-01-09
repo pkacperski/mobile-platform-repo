2da8e1bc505ff36010edc1db3e812a2aa0438ac4
pkacperski <przemyslaw.kacperski@interia.pl>
2022-01-10 00:39:11 +0100

    Add function testing OpenCV streaming and configuration instructions

--- mobileplatform-backend/src/main/java/com/mobileplatform/backend/MobileplatformBackendApplication.java
+++ mobileplatform-backend/src/main/java/com/mobileplatform/backend/MobileplatformBackendApplication.java
@@ -1,5 +1,6 @@
 package com.mobileplatform.backend;
 
+import com.mobileplatform.backend.opencv.OpenCVTest;
 import com.mobileplatform.backend.websocket.WebSocketBackendServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileplatformBackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MobileplatformBackendApplication.class, args);
-		WebSocketBackendServer.initialize();
+//		SpringApplication.run(MobileplatformBackendApplication.class, args);
+//		WebSocketBackendServer.initialize();
+		OpenCVTest.testOpenCV();
 	}
 }
