5344f63cf47066fdb54dfc039726a0eb56956490
pkacperski <przemyslaw.kacperski@interia.pl>
2022-01-14 13:55:14 +0100

    Add WIP impl of saving received video streams to avi files

--- mobileplatform-backend/src/main/java/com/mobileplatform/backend/MobileplatformBackendApplication.java
+++ mobileplatform-backend/src/main/java/com/mobileplatform/backend/MobileplatformBackendApplication.java
@@ -1,7 +1,7 @@
 package com.mobileplatform.backend;
 
-import com.mobileplatform.backend.opencv.VideoCaptureImpl;
 import com.mobileplatform.backend.opencv.VideoCaptureHandler;
+import com.mobileplatform.backend.opencv.VideoCaptureImpl;
 import com.mobileplatform.backend.websocket.TelemetryServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +14,15 @@ public class MobileplatformBackendApplication {
 	public final static String WEBSOCKET_SERVER_IP_ADDRESS = "localhost";
 	public final static int TELEMETRY_SERVER_PORT_NUMBER = 8081; // by default, port 8080 used for serving backend services, 8081 - for telemetry, 8082 and above for video streams
 	public final static int[] VIDEO_STREAMS_PORT_NUMBERS = {8082, 8083}; // possible to add more ports to handle more video streams
-	// TODO - docelowe adresy streamow:
-	final static String[] STREAMS_ADDRESSES = {"0", "C:/Users/DELL/Documents/Mgr/rozne/video-star.mp4", "C:/Users/DELL/Documents/Mgr/rozne/video-12angrymen.mp4",
-			"C:/Users/DELL/Documents/Mgr/rozne/video-intouchables.avi",	"C:/Users/DELL/Documents/Mgr/rozne/video-theory.mp4",
-			"C:/Users/DELL/Documents/Mgr/rozne/video-harry.mkv", "C:/Users/DELL/Documents/Mgr/rozne/video-beauty.mp4"};
+	final static String[] STREAMS_ADDRESSES = { // TODO - docelowe adresy streamow:
+			"0",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-star.mp4",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-12angrymen.mp4",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-intouchables.avi",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-theory.mp4",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-beauty.mp4",
+			"C:/Users/DELL/Documents/Mgr/rozne/video-harry.mkv"
+	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileplatformBackendApplication.class, args); // supposedly no need to create a new thread
--- mobileplatform-backend/src/main/java/com/mobileplatform/backend/opencv/VideoCaptureImpl.java
+++ mobileplatform-backend/src/main/java/com/mobileplatform/backend/opencv/VideoCaptureImpl.java
@@ -8,7 +8,11 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /*
  * Test of OpenCV module dependencies added manually to IntelliJ instead of importing a Maven dependency.
@@ -26,38 +30,55 @@ public class VideoCaptureImpl implements Runnable {
     private final String streamAddress;
     private final VideoServer videoServer;
     private boolean isStreamActive;
+    private static String dateTimeNow; // TODO - stworzyc folder z data w nazwie: https://stackoverflow.com/questions/28947250/create-a-directory-if-it-does-not-exist-and-then-create-the-files-in-that-direct
 
     public VideoCaptureImpl(String streamAddress, VideoServer videoServer, boolean isStreamActive) {
         this.streamAddress = streamAddress;
         this.videoServer = videoServer;
         this.isStreamActive = isStreamActive;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm:ss");
+        dateTimeNow = dateTimeFormatter.format(LocalDateTime.now());
     }
 
     public static void initialize() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("Loaded OpenCV: " + Core.getVersionString());
+        // TODO - check: no need to load or import openh264-1.8.0-win64.dll file to save videos to .avi files with VideoWriter?
     }
 
     public void run() {
         final String BMP_FILE_EXTENSION = ".bmp";
-        // TODO - remove webcam test:
         VideoCapture videoCapture = this.streamAddress.equals("0") ? new VideoCapture(0) : new VideoCapture(this.streamAddress, Videoio.CAP_FFMPEG);
+        int fourcc = VideoWriter.fourcc('M', 'J', 'P', 'G');
+        double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
+        // Size originalSize = new Size(640, 360); // TODO - rozmiar video do zapisu
+        Size originalSize = new Size((int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH), (int) videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT));
         Mat frame = new Mat();
 
-        while(videoCapture.read(frame)) {
-            if(isStreamActive) {
-                if(frame.width() > 0 && frame.height() > 0) {
-                    Mat resizedFrame = new Mat();
-                    Size size = new Size(400,300); // TODO - set correct size of images before sending to FE?
-                    Imgproc.resize(frame, resizedFrame, size);
+        // TODO - nazwy plikow + stworzyc nowy folder z data w nazwie: https://stackoverflow.com/questions/28947250/create-a-directory-if-it-does-not-exist-and-then-create-the-files-in-that-direct
+        VideoWriter videoWriter = new VideoWriter(
+                "C:\\Users\\DELL\\Documents\\Mgr\\mobile-platform-repo\\mobile-platform-repo\\saved-videos\\" /*+ dateTimeNow */ + "video-test-" + ((this.streamAddress.length() > 10) ? this.streamAddress.substring(this.streamAddress.length() - 10, this.streamAddress.length() - 4) : "webcam") + ".avi",
+                fourcc, fps, originalSize, true); // TODO - resizing image originalSize here?
 
+        while(videoCapture.read(frame)) {
+            if(frame.width() > 0 && frame.height() > 0) {
+                // proba resize
+                //Mat resizedFrameSaving = new Mat();
+                //Imgproc.resize(frame, resizedFrameSaving, originalSize);
+                // /proba resize
+                videoWriter.write(frame);
+                if(isStreamActive) {
+                    Mat resizedFrameFrontend = new Mat();
+                    Size sizeFrontend = new Size(400,300); // TODO - set correct size of images before sending to FE? + maintain original video proportions?
+                    Imgproc.resize(frame, resizedFrameFrontend, sizeFrontend);
                     MatOfByte frameOfByte = new MatOfByte();
-                    Imgcodecs.imencode(BMP_FILE_EXTENSION, resizedFrame, frameOfByte);
+                    Imgcodecs.imencode(BMP_FILE_EXTENSION, resizedFrameFrontend, frameOfByte);
                     this.videoServer.send(frameOfByte.toArray());
                 }
             }
         }
         videoCapture.release();
+        videoWriter.release();
     }
 
     public void setStreamActive(boolean isStreamActive) {
