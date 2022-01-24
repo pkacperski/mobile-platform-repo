dfc3671e2eba51353ca86a997d555e3c781bc38e
pkacperski <przemyslaw.kacperski@interia.pl>
2022-01-24 16:24:06 +0100

    WIP - added some non-working tries to visualise location with GridPane component

--- mobileplatform-frontend/src/main/java/com/mobileplatform/frontend/MobileplatformFrontend.java
+++ mobileplatform-frontend/src/main/java/com/mobileplatform/frontend/MobileplatformFrontend.java
@@ -27,6 +27,7 @@ public class MobileplatformFrontend {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             Objects.requireNonNull(ActionsFactory.getActions("MainForm")).control();
+//            MainFormActions.getInstance().getMainForm().getPanelLocationCoordinateSystem().add(new MainForm.GridPane()); // TODO - NPE -> trzeba dodac do JFrame i mozna tylko raz dodac content?
 
             Thread telemetryClientThread = new Thread(TelemetryClient::initialize);
             telemetryClientThread.start();
