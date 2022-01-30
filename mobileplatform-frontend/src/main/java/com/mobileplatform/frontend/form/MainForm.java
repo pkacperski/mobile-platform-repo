29a7ffb8806ac8a46a7d4f8ad89e45be08dd20ee
pkacperski <przemyslaw.kacperski@interia.pl>
2022-01-30 20:03:06 +0100

    Add thick borders to buttons

--- mobileplatform-frontend/src/main/java/com/mobileplatform/frontend/form/MainForm.java
+++ mobileplatform-frontend/src/main/java/com/mobileplatform/frontend/form/MainForm.java
@@ -3,12 +3,14 @@ package com.mobileplatform.frontend.form;
 import lombok.Getter;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 @Getter
 public class MainForm {
     private JFrame frame;
     private JPanel panel;
+    private JPanel panelVehicle1;
     private JButton btnFetchData;
     private JLabel lblVehicleName;
     private JLabel lblAccelerometerReading;
@@ -16,7 +18,6 @@ public class MainForm {
     private JLabel lblMagnetometerReading;
     private JLabel lblVideoStream;
     private JTabbedPane tabbedPane;
-    private JPanel panelVehicle1;
     private JButton btnEmergencyStop;
     private JButton btnEmergencyAbort;
     private JButton btnAutonomousDrivingMode;
@@ -27,6 +28,23 @@ public class MainForm {
     private JLabel lblVehicleIp;
     private JLabel lblVehicleId;
     private JButton btnDisconnectVehicle;
+    private JButton btnStream1;
+    private JButton btnStream2;
+    private JButton btnStream3;
+    private JProgressBar progressBarBatteryStatus;
+    private JProgressBar progressBarWheelsTurnLeft;
+    private JProgressBar progressBarWheelsTurnRight;
+    private JProgressBar progressBarCamerasTurnLeft;
+    private JProgressBar progressBarCamerasTurnRight;
+    private JProgressBar progressBarLeftFrontWheelSpeed;
+    private JProgressBar progressBarLeftRearWheelSpeed;
+    private JProgressBar progressBarRightFrontWheelSpeed;
+    private JProgressBar progressBarRightRearWheelSpeed;
+    private JButton btnShowLocationHistory;
+    private JButton btnShowPointCloud;
+    private JButton btnShowLidarOccupancyMap;
+    private JButton btnOpenVehicle2View;
+    private JPanel panelVehicle2;
     private JPanel panelVehicle2Deprecated;
     private JLabel lblEncoderReadingVehicle2;
     private JButton btnFetchDataVehicle2;
@@ -47,26 +65,9 @@ public class MainForm {
     private JButton btnManualSteeringModeVehicle2;
     private JLabel lblImuReadingVehicle2;
     private JButton btnDisconnectVehicle2;
-    private JButton btnStream2;
-    private JButton btnStream1;
-    private JButton btnStream3;
     private JButton btnStream1Vehicle2;
     private JButton btnStream2Vehicle2;
     private JButton btnStream3Vehicle2;
-    private JProgressBar progressBarBatteryStatus;
-    private JProgressBar progressBarWheelsTurnLeft;
-    private JProgressBar progressBarWheelsTurnRight;
-    private JProgressBar progressBarCamerasTurnLeft;
-    private JProgressBar progressBarCamerasTurnRight;
-    private JProgressBar progressBarLeftFrontWheelSpeed;
-    private JProgressBar progressBarLeftRearWheelSpeed;
-    private JProgressBar progressBarRightFrontWheelSpeed;
-    private JProgressBar progressBarRightRearWheelSpeed;
-    private JButton btnShowLocationHistory;
-    private JButton btnShowPointCloud;
-    private JButton btnShowLidarOccupancyMap;
-    private JButton btnOpenVehicle2View;
-    private JPanel panelVehicle2;
 
     public MainForm() {
         frame = new JFrame("MainForm");
@@ -76,6 +77,30 @@ public class MainForm {
         frame.pack();
         progressBarWheelsTurnLeft.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         progressBarCamerasTurnLeft.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        setButtonsBorders();
+    }
+
+    private void setButtonsBorders() {
+        final Color buttonBorderColor = new Color(128, 128, 128);
+        final int thicknessFive = 4;
+        final boolean roundedCornersFalse = false;
+        final LineBorder buttonBorder = new LineBorder(buttonBorderColor, thicknessFive, roundedCornersFalse);
+
+        btnFetchData.setBorder(buttonBorder);
+        btnEmergencyStop.setBorder(buttonBorder);
+        btnEmergencyAbort.setBorder(buttonBorder);
+        btnAutonomousDrivingMode.setBorder(buttonBorder);
+        btnManualSteeringMode.setBorder(buttonBorder);
+        btnConnectVehicle.setBorder(buttonBorder);
+        btnDisconnectVehicle.setBorder(buttonBorder);
+        btnConnectVehicle.setBorder(buttonBorder);
+        btnStream1.setBorder(buttonBorder);
+        btnStream2.setBorder(buttonBorder);
+        btnStream3.setBorder(buttonBorder);
+        btnShowLocationHistory.setBorder(buttonBorder);
+        btnShowPointCloud.setBorder(buttonBorder);
+        btnShowLidarOccupancyMap.setBorder(buttonBorder);
+        btnOpenVehicle2View.setBorder(buttonBorder);
     }
 
     private void createUIComponents() {
